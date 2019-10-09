f4894ff4f7ca32a2a5055bff18c833e3088efde3
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-08 19:29:56 -0600

    Update api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 const merge = require('lodash/merge');
 
 const permission = require('./permission');
+const role = require('./role');
 
-const typeDefs = [permission.typeDefs].join(' ');
+const typeDefs = [permission.typeDefs, role.typeDefs].join(' ');
 
-const resolvers = merge({}, permission.resolvers);
+const resolvers = merge({}, permission.resolvers, role.resolvers);
 
 module.exports = {
   typeDefs,
