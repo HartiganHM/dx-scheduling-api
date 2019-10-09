90f81769f5d8597474b999e6b324bca24594d6bf
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-08 19:52:10 -0600

    Add users to api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,11 @@ const merge = require('lodash/merge');
 
 const permission = require('./permission');
 const role = require('./role');
+const user = require('./user');
 
-const typeDefs = [permission.typeDefs, role.typeDefs].join(' ');
+const typeDefs = [permission.typeDefs, role.typeDefs, user.typeDefs].join(' ');
 
-const resolvers = merge({}, permission.resolvers, role.resolvers);
+const resolvers = merge({}, permission.resolvers, role.resolvers, user.resolvers);
 
 module.exports = {
   typeDefs,
