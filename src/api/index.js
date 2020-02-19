db59d6df326462055b4cd4dd666e96c5db4859b9
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-02-19 10:23:15 -0700

    Add Client type to API

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,26 @@
 const merge = require('lodash/merge');
 
+const client = require('./client');
 const permission = require('./permission');
 const role = require('./role');
 const user = require('./user');
 
-const typeDefs = [permission.typeDefs, role.typeDefs, user.typeDefs].join(' ');
+const typeDefs = [
+  client.typeDefs,
+  permission.typeDefs,
+  role.typeDefs,
+  user.typeDefs,
+].join(' ');
 
-const resolvers = merge({}, permission.resolvers, role.resolvers, user.resolvers);
+const resolvers = merge(
+  {},
+  client.resolvers,
+  permission.resolvers,
+  role.resolvers,
+  user.resolvers
+);
 
 module.exports = {
   typeDefs,
-  resolvers
+  resolvers,
 };
