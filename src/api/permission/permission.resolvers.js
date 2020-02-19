4b89e9cb1e37d82cd23fc2394638e15d978579bc
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-02-19 10:23:40 -0700

    Refactor and fix linting for permission resolvers

--- src/api/permission/permission.resolvers.js
+++ src/api/permission/permission.resolvers.js
@@ -1,28 +1,29 @@
-const permission = (parent, { input: { id } }, context) => {
-  return context.prisma.permission({ id });
-};
+const permission = (parent, { input: { id } }, context) =>
+  context.prisma.permission({ id });
 
-const permissions = (parent, args, context) => {
-  return context.prisma.permissions();
-}; 
+const permissions = (parent, args, context) => context.prisma.permissions();
 
 const createPermission = (parent, { input: { name, roles } }, context) => {
   return context.prisma.createPermission({ name, roles });
 };
 
-const updatePermission = (parent, { input: { name }, where: { id } }, context) => {
-  return context.prisma.updatePermission({ data: { name }, where: { id }});
-}
+const updatePermission = (
+  parent,
+  { input: { name }, where: { id } },
+  context
+) => {
+  return context.prisma.updatePermission({ data: { name }, where: { id } });
+};
 
 const permissionResolvers = {
   Query: {
     permission,
-    permissions
+    permissions,
   },
   Mutation: {
     createPermission,
     updatePermission,
-  }
-}
+  },
+};
 
 module.exports = permissionResolvers;
