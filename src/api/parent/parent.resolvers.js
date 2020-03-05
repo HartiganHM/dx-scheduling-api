1a3ecc80c8d030161df407ceb6f4db02059856b1
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-03-04 18:14:32 -0700

    Refactor Parent resolvers

--- src/api/parent/parent.resolvers.js
+++ src/api/parent/parent.resolvers.js
@@ -2,15 +2,14 @@ const parent = (parent, { input }, context) => context.prisma.parent(input);
 
 const parents = (parent, args, context) => context.prisma.parents();
 
-const createParent = async (parent, { input }, context) =>
+const createParent = (parent, { input }, context) =>
   context.prisma.createParent(input);
 
 const deleteParent = (parent, { where }, context) =>
   context.prisma.deleteParent(where);
 
-const updateParent = (parent, { input, where }, context) => {
-  return context.prisma.updateParent({ data: input, where });
-};
+const updateParent = (parent, { input, where }, context) =>
+  context.prisma.updateParent({ data: input, where });
 
 const parentResolvers = {
   Query: {
