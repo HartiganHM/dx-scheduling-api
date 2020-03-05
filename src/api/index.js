12f4d11d0d90b9eb0a1b67bdb3a374a4b996ea69
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-03-04 18:17:16 -0700

    Add Insurance type to api/index

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const merge = require('lodash/merge');
 
 const address = require('./address');
 const client = require('./client');
+const insurance = require('./insurance');
 const parent = require('./parent');
 const permission = require('./permission');
 const physician = require('./physician');
@@ -11,6 +12,7 @@ const user = require('./user');
 const typeDefs = [
   address.typeDefs,
   client.typeDefs,
+  insurance.typeDefs,
   parent.typeDefs,
   permission.typeDefs,
   physician.typeDefs,
@@ -22,6 +24,7 @@ const resolvers = merge(
   {},
   address.resolvers,
   client.resolvers,
+  insurance.resolvers,
   parent.resolvers,
   permission.resolvers,
   physician.resolvers,
