a407f520a77c67684f2f922e81ce44f270a6ba64
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-03-07 08:47:55 -0700

    Add Diagnosis to api/index

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ const merge = require('lodash/merge');
 const address = require('./address');
 const client = require('./client');
 const concerns = require('./concerns');
+const diagnosis = require('./diagnosis');
 const intakeFormValues = require('./intakeFormValues');
 const insurance = require('./insurance');
 const parent = require('./parent');
@@ -15,6 +16,7 @@ const typeDefs = [
   address.typeDefs,
   client.typeDefs,
   concerns.typeDefs,
+  diagnosis.typeDefs,
   intakeFormValues.typeDefs,
   insurance.typeDefs,
   parent.typeDefs,
@@ -29,6 +31,7 @@ const resolvers = merge(
   address.resolvers,
   client.resolvers,
   concerns.resolvers,
+  diagnosis.resolvers,
   intakeFormValues.resolvers,
   insurance.resolvers,
   parent.resolvers,
